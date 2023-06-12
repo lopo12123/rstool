2c6debb2fa5b60221f45af7d3773a6f94c89069a
lopo <lopo@zju.edu.cn>
2023-06-13 00:01:51 +0800

    feat: serve 参数解析 + single 模式启动（部分）

--- src/serve.rs
+++ src/serve.rs
@@ -1,12 +1,15 @@
 use std::env::current_dir;
-use std::fmt::format;
-use std::fs;
 use std::path::{Path, PathBuf};
 use ipconfig::get_adapters;
-use rocket::Config;
+use rocket::{Build, Config, get, launch, Rocket, routes};
+use rocket::fs::NamedFile;
+use rocket::http::ext::Normalize;
+use rocket::tokio::runtime::Runtime;
 
+#[derive(Debug)]
 enum ServerMode { Single, Mixed, Direct }
 
+#[derive(Debug)]
 struct ServerBuilder {
     root: PathBuf,
     entry: PathBuf,
@@ -15,14 +18,8 @@ struct ServerBuilder {
 }
 
 impl ServerBuilder {
-    /// root: 存在的目录
-    fn check_root(root: &str) -> bool {
-        match fs::metadata(root) {
-            Ok(meta) => meta.is_dir(),
-            Err(_) => false
-        }
-    }
-    /// root: 存在的目录
+    // region 参数解析
+    /// root: 服务根目录
     fn get_root(root: &str) -> Result<PathBuf, String> {
         let path_root = PathBuf::from(root);
 
@@ -32,60 +29,79 @@ impl ServerBuilder {
             Err(format!("invalid root (root is not a directory)"))
         } else {
             if path_root.is_absolute() {
-                Ok(path_root)
+                Ok(path_root.canonicalize().unwrap())
             } else {
                 match current_dir() {
-                    Ok(curr) => Ok(curr.join(path_root)),
+                    Ok(curr) => Ok(curr.join(path_root).canonicalize().unwrap()),
                     Err(_) => Err(format!("unable to resolve the absolute path of the current location"))
                 }
             }
         }
     }
-    /// entry: 存在的文件
-    fn check_entry(entry: &str) -> bool {
-        match fs::metadata(entry) {
-            Ok(meta) => meta.is_file(),
-            Err(_) => false
-        }
-    }
-    /// port: 1024 ~ 65535
-    fn check_port(port: u16) -> bool {
-        port >= 1024
-    }
 
-    /// mode: 'single' | 'mixed' | 'direct'
-    fn get_mode(mode: &str) -> Option<ServerMode> {
-        match mode {
-            "single" => Some(ServerMode::Single),
-            "mixed" => Some(ServerMode::Mixed),
-            "direct" => Some(ServerMode::Direct),
-            _ => None
-        }
-    }
+    /// entry: 入口文件
+    fn get_entry(root: PathBuf, entry: &str) -> Result<PathBuf, String> {
+        let path_entry = root.join(entry);
 
-
-    /// 解析并校验参数, 构建服务器实例
-    pub fn try_build(root: String, entry: String, port: u16, mode: String) -> Result<Self, String> {
-        if !ServerBuilder::check_root(&root) {
-            Err(format!("invalid root (root does not exist or is not a directory)"))
-        } else if !ServerBuilder::check_entry(&entry) {
-            Err(format!("invalid entry (entry does not exist or is not a file)"))
-        } else if !ServerBuilder::check_port(port) {
-            Err(format!("invalid port (required: 1024 ~ 65535, received: {port})"))
+        if !path_entry.exists() {
+            Err(format!("invalid entry (entry does not exist)"))
+        } else if !path_entry.is_file() {
+            Err(format!("invalid entry (entry is not a file)"))
         } else {
-            match ServerBuilder::get_mode(&mode.to_lowercase()) {
-                Some(m) => {
-                    // todo
-                    Err(format!("invalid port (required: 1024 ~ 65535, received: {port})"))
+            if path_entry.is_absolute() {
+                Ok(path_entry.canonicalize().unwrap())
+            } else {
+                match current_dir() {
+                    Ok(curr) => Ok(curr.join(path_entry).canonicalize().unwrap()),
+                    Err(_) => Err(format!("unable to resolve the absolute path of the entry file"))
                 }
-                None => Err(format!("invalid mode (required: 'single' / 'mixed' / 'direct', received: {port})"))
             }
         }
     }
 
-    pub fn start(&self) {
-        // todo
+    /// port: 1024 ~ 65535
+    fn get_port(port: u16) -> Result<u16, String> {
+        if port < 1024 {
+            Err(format!("invalid port (required: 1024 ~ 65535, received: {port})"))
+        } else {
+            Ok(port)
+        }
     }
+
+    /// mode: 'single' | 'mixed' | 'direct'
+    fn get_mode(mode: &str) -> Result<ServerMode, String> {
+        match mode {
+            "single" => Ok(ServerMode::Single),
+            "mixed" => Ok(ServerMode::Mixed),
+            "direct" => Ok(ServerMode::Direct),
+            _ => Err(format!("invalid mode (required: 'single' / 'mixed' / 'direct', received: {mode})"))
+        }
+    }
+    // endregion
+
+    /// 解析并校验参数, 构建服务器实例
+    pub fn try_build(root: String, entry: String, port: u16, mode: String) -> Result<Self, String> {
+        let _root = ServerBuilder::get_root(&root)?;
+        println!("[try_build] root: {:?}", _root.normalized_str());
+        let _entry = ServerBuilder::get_entry(_root.clone(), &entry)?;
+        println!("[try_build] entry: {:?}", _entry.normalized_str());
+        let _port = ServerBuilder::get_port(port)?;
+        println!("[try_build] port: {port}");
+        let _mode = ServerBuilder::get_mode(&mode)?;
+        println!("[try_build] mode: {_mode:?}");
+
+        Ok(ServerBuilder {
+            root: _root,
+            entry: _entry,
+            port: _port,
+            mode: _mode,
+        })
+    }
+}
+
+#[get("/")]
+fn index_single(my_str: &rocket::State<String>) -> String {
+    my_str.to_string()
 }
 
 pub struct ServeImpl {}
@@ -107,6 +123,22 @@ impl ServeImpl {
         ips
     }
 
+    // region 三种启动模式
+    /// 单一模式
+    // #[launch]
+    async fn single() {
+        let my_str = "自定义参数";
+        rocket::build()
+            .manage(my_str.to_string())
+            .mount("/", routes![index_single])
+            .launch().await.ok();
+    }
+    /// 混合模式
+    fn mixed() {}
+    /// 直接模式
+    fn direct() {}
+    // endregion
+
     /// 处理 Command::Serve 子命令
     pub fn handle(root: String, entry: String, port: u16, mode: String) {
         println!("[Commands::Serve] root: '{root}' entry: '{entry}', port: '{port}', mode: '{mode}'");
@@ -114,10 +146,18 @@ impl ServeImpl {
         match ServerBuilder::try_build(root, entry, port, mode) {
             Ok(server) => {
                 for ip in ServeImpl::get_ips() {
-                    println!("http://{ip}:{port}");
+                    println!("[serve_at] http://{ip}:{port}/");
                 }
 
-                server.start();
+                match server.mode {
+                    ServerMode::Single => {
+                        Runtime::new()
+                            .unwrap()
+                            .block_on(ServeImpl::single());
+                    }
+                    ServerMode::Mixed => ServeImpl::mixed(),
+                    ServerMode::Direct => ServeImpl::direct(),
+                }
             }
             Err(err) => println!("Error: {err}"),
         }
@@ -129,6 +169,7 @@ mod unit_test {
     use std::path::Path;
     use super::*;
     use ipconfig::{get_adapters, Adapter};
+    use rocket::http::ext::Normalize;
 
     #[test]
     fn ips() {
@@ -148,7 +189,7 @@ mod unit_test {
 
     #[test]
     fn ppp() {
-        let p = ".";
-        println!("{}, {}", Path::new("D:/").is_absolute(), Path::new("H:/").is_absolute());
+        let p = "D:\\rstool\\target\\debug\\../../examples";
+        println!("{:?}", Path::new(p).canonicalize().unwrap());
     }
 }
