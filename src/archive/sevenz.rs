f60537d5ea66cf95bcdde4b7338bc86a7e6fc2ef
lopo <lopo@zju.edu.cn>
2023-08-27 21:42:17 +0800

    todo: 7z pack

--- src/archive/sevenz.rs
+++ src/archive/sevenz.rs
@@ -1,5 +1,6 @@
 use std::io::Cursor;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use crate::archive::utils::ArchiveEntry;
 
 pub fn extract_sevenz(sevenz_buffer: Vec<u8>, dest: &Path) -> Result<(), String> {
     match sevenz_rust::decompress(Cursor::new(sevenz_buffer), dest) {
@@ -8,7 +9,14 @@ pub fn extract_sevenz(sevenz_buffer: Vec<u8>, dest: &Path) -> Result<(), String>
     }
 }
 
-pub fn pack() {}
+pub fn pack(binary: Vec<u8>, disk_root: impl Into<PathBuf>) -> Vec<ArchiveEntry>{
+    let mut entries = vec![];
+    let disk_root = disk_root.into();
+
+    // TODO: 7z pack
+
+    entries
+}
 
 #[cfg(test)]
 mod unit_test {}
