697dbe936cbf7f0b9871a616600871bcab79116a
lopo <lopo@zju.edu.cn>
2023-09-02 22:35:30 +0800

    feat + test: sevenz::pack

--- src/archive/sevenz.rs
+++ src/archive/sevenz.rs
@@ -1,16 +1,8 @@
 use std::cmp::Ordering;
 use std::io::{Cursor};
 use std::path::{Path, PathBuf};
-use sevenz_rust::Password;
+use sevenz_rust::{Password, SevenZArchiveEntry};
 use crate::archive::utils::ArchiveEntry;
-
-pub fn extract_sevenz(sevenz_buffer: Vec<u8>, dest: &Path) -> Result<(), String> {
-    match sevenz_rust::decompress(Cursor::new(sevenz_buffer), dest) {
-        Ok(_) => Ok(()),
-        Err(err) => Err(format!("{err}")),
-    }
-}
-
 
 /// 将 7z 解包为 `ArchiveEntry` 列表, 返回 `ArchiveEntry` 列表 (按照文件夹优先, 文件次之的顺序排序)
 pub fn unpack(binary: Vec<u8>, disk_root: String) -> Vec<ArchiveEntry> {
@@ -54,21 +46,26 @@ pub fn unpack(binary: Vec<u8>, disk_root: String) -> Vec<ArchiveEntry> {
     entries
 }
 
-pub fn pack(binary: Vec<u8>, disk_root: impl Into<PathBuf>) -> Vec<ArchiveEntry> {
-    // let mut entries = vec![];
-    // // let disk_root = disk_root.into();
-    //
-    // // TODO: 7z pack
-    //
-    // entries
+/// 将 `ArchiveEntry` 列表打包为 7z, 返回二进制数据
+pub fn pack(entries: Vec<ArchiveEntry>) -> Vec<u8> {
+    let mut bundle = sevenz_rust::SevenZWriter::new(Cursor::new(vec![])).unwrap();
 
-    todo!()
+    for entry in entries {
+        let mut item = SevenZArchiveEntry::default();
+        item.name = entry.pack_dir.clone();
+        item.is_directory = !entry.is_file;
+
+        bundle.push_archive_entry(item, entry.raw.map(|bytes| Cursor::new(bytes))).unwrap();
+    }
+
+    bundle.finish().unwrap().into_inner()
 }
 
 #[cfg(test)]
 mod unit_test {
     use super::*;
     use std::fs;
+    use crate::archive::utils::ArchiveBuilder;
 
     /// unpack 函数测试
     #[test]
@@ -87,5 +84,19 @@ mod unit_test {
             //     fs::create_dir_all(entry.disk_dir).unwrap();
             // }
         }
+    }
+
+    /// pack 函数测试
+    #[test]
+    fn pack_test() {
+        // let base = r"C:\Users\20366\Desktop\misc_test\zip";
+        let base = r"C:\Users\20366\Desktop\misc";
+        let items = vec!["folder".into()];
+
+        let entries = ArchiveBuilder::build(base.into(), items).get_entries();
+        let package = pack(entries);
+
+        fs::write(r"C:\Users\20366\Desktop\misc\folder.7z", package).unwrap();
+        println!("done!");
     }
 }
