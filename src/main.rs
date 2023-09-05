453381a7ef9055b083034280314a9d8770708ab7
lopo <lopo@zju.edu.cn>
2023-09-05 23:44:53 +0800

    feat: resolve todo

--- src/main.rs
+++ src/main.rs
@@ -16,29 +16,32 @@ mod fontmin;
 mod image;
 
 fn main() {
-    // TODO: use try_parse instead, to avoid panic on debug mode
-    let cmd = Cli::parse().command;
-
-    match cmd {
-        Commands::Doc => {
-            DocImpl::handle();
+    // use try_parse instead, to avoid panic on debug mode
+    match Cli::try_parse() {
+        Ok(Cli { command }) => match command {
+            Commands::Doc => {
+                DocImpl::handle();
+            }
+            Commands::Hash { source, filemode, algorithm } => {
+                HashImpl::handle(source, filemode, algorithm);
+            }
+            Commands::Image { source, format, size } => {
+                ImageImpl::handle(source, format, size);
+            }
+            Commands::Pack { destination, sources } => {
+                PackImpl::handle(current_dir().unwrap(), destination, sources);
+            }
+            Commands::Serve { root, entry, port, mode } => {
+                ServeImpl::handle(root, entry, port, mode);
+            }
+            Commands::Unpack { source, destination } => {
+                UnpackImpl::handle(source, destination);
+            }
+            // Commands::FontMin { input, output, chars } => FontMinImpl::handle(input, output, chars),
+            // _ => println!("Not yet implemented!")
         }
-        Commands::Hash { source, filemode, algorithm } => {
-            HashImpl::handle(source, filemode, algorithm);
+        Err(err) => {
+            println!("{err}");
         }
-        Commands::Image { source, format, size } => {
-            ImageImpl::handle(source, format, size);
-        }
-        Commands::Pack { destination, sources } => {
-            PackImpl::handle(current_dir().unwrap(), destination, sources);
-        }
-        Commands::Serve { root, entry, port, mode } => {
-            ServeImpl::handle(root, entry, port, mode);
-        }
-        Commands::Unpack { source, destination } => {
-            UnpackImpl::handle(source, destination);
-        }
-        // Commands::FontMin { input, output, chars } => FontMinImpl::handle(input, output, chars),
-        // _ => println!("Not yet implemented!")
     }
 }
